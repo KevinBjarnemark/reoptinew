54c0ee03618dcfa39043b6f34da960ed6b819303
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-12-19 19:39:04 +0100

    Basic configuration: Installed React (With Vite + legacy plugin) and added a favicon

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+      legacy({
+        targets: [
+          'ie 11',
+          'chrome 49',
+          'firefox 52',
+          'safari 9',
+          'ios_saf 9',
+        ],
+      })
+  ],
+})
