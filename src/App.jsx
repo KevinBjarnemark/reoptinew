81b966f6ab76de3b642aeade0bd0b5d108d106e5
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-12-27 16:02:50 +0100

    Added global styling, custom Bootstrap utilities, self-hosted fonts, and a basic routed page template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import './App.css'
+import { lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import imgLogo from "./assets/images/brand/logo.webp";
+
+// Load pages lazily
+const SignUp = lazy(() => import('./pages/signup/SignUp'));
+
+// Header
+const Header = () => {
+    return (
+      <header>
+          <img 
+              style={{
+                  width: "80px",
+                  aspectRatio: "578 / 512",
+              }}
+              src={imgLogo}>
+          </img>
+      </header>
+    );
+}
+
+// Navigation
+const Navigation = () => {
+    return (
+      <nav></nav>
+    );
+}
 
 function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <main>
+                <article>
+                    <Router>
+                        <Routes>
+                            <Route path="/signup" element={<SignUp />}></Route>
+                        </Routes>
+                    </Router>
+                </article>
+            </main>
+            <Navigation />
+        </>
+    )
 }
 
 export default App
