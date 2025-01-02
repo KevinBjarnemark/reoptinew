998e47b6537d72df31f26e7163473fbfb5e5b8c2
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2025-01-02 10:38:32 +0100

    Added a name/identifier to the front end logging system

--- src/utils/log.js
+++ src/utils/log.js
@@ -3,14 +3,19 @@
  * Logs debugging messages to the console in development.
  * 
  * @param {bool}  log Extra conditional for toggling component debugging.
+ * @param {string}  name Name/identifier of the log
  * @param {...any}  args Accepts any number of arguments of any type.
  */
-export const debug = (log, ...args) => {
+export const debug = (log, name, ...args) => {
     // Log to the console if the .env.development file is in use
     if (log && import.meta.env.MODE === "development") {
         console.log(
-            "%cDEBUG:",
+            "%cDEBUG:%c %c%s:%c",
             "color: #61b5f1; font-weight: bold;", 
+            "", // Reset style
+            "color: #5a5f61; font-weight: bold;",
+            name,
+            "", // Reset style
             ...args);
     }
 };
@@ -19,14 +24,19 @@ export const debug = (log, ...args) => {
  * Logs messages to the console in development.
  * 
  * @param {bool}  log Extra conditional for toggling component debugging.
+ * @param {string}  name Name/identifier of the log
  * @param {...any}  args Accepts any number of arguments of any type.
  */
-export const log = (log, ...args) => {
+export const log = (log, name, ...args) => {
     // Log to the console if the .env.development file is in use
-    if (import.meta.env.MODE === "development") {
+    if (log && import.meta.env.MODE === "development") {
         console.log(
-            "%cLOG:",
+            "%cLOG:%c %c%s:%c",
             "color: #f1ba61; font-weight: bold;", 
+            "", // Reset style
+            "color: #5a5f61; font-weight: bold;",
+            name,
+            "", // Reset style
             ...args);
     }
 };
